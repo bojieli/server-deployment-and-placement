ec4e5ddb3dcaa12eccb25967f65dd8b4614f6314
Jiaying Meng <mengjy313@gmail.com>
2019-01-08 19:58:58 +0800

    Str2arr

--- main/GeneralCase.java
+++ main/GeneralCase.java
@@ -4,15 +4,18 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*；
+import java.util.*;
 
 import org.graphstream.graph.ElementNotFoundException;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.GraphParseException;
+
+import com.alibaba.fastjson.JSONArray;
 
 import appro.*;
 import configuration.*;
@@ -55,8 +58,28 @@ public class GeneralCase {
     	}
     	return new OutputResult(benefit,opencost);
 	}
+	
+	public static ArrayList<Integer> str1arr(Object input) {
+	    JSONArray jsonArray = JSONArray.parseArray(String.valueOf(input));
+	    ArrayList<Integer> arrayList = new ArrayList<>();
+	    for (Iterator<Object> it = jsonArray.iterator(); it.hasNext(); ) {
+	        Integer integer = (Integer) it.next();
+	        arrayList.add(integer);
+	    }
+	    return arrayList;
+	}
+    
+    public static ArrayList<Double> str2arr(Object input) {
+    	   JSONArray jsonArray = JSONArray.parseArray(String.valueOf(input));
+    	   ArrayList<Double> arrayList = new ArrayList<>();
+    	   for (Iterator<Object> it = jsonArray.iterator(); it.hasNext(); ) {
+    	      Double aDouble =  ((BigDecimal)it.next()).doubleValue();
+    	      arrayList.add(aDouble);
+    	   }
+    	   return arrayList;
+    	}
 
-	public void run(Graph graph,ArrayList<Double> capacity,ArrayList<Integer> k,ArrayList<Double> costlist,String filename) throws Exception{
+	public void run(Graph graph,ArrayList<Double> capacity,ArrayList<Integer> k,ArrayList<Double> costlist,String filename) throws IOException{
 		
 		/*File f=new File(filename);
         f.createNewFile();
@@ -73,7 +96,7 @@ public class GeneralCase {
 		long count = 0;
 		for(int i=0;i<graph.getNodeCount();i++){
 			 Node node = graph.getNode(i);
-			 ArrayList<Integer> list = (ArrayList<Integer>)node.getAttribute("type");
+			 ArrayList<Integer> list = str1arr(node.getAttribute("type"));
 			 count += list.size();
 		}
 
@@ -98,7 +121,7 @@ public class GeneralCase {
 	public static void main(String arg[]) { 
 		Graph graph = new SingleGraph("network");
 		try {
-			graph.read("ordinary_graph.dgs");
+			graph.read(parameters_generator.AP_NUM+"AP_graph.dgs");
 		} catch (ElementNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -106,18 +129,34 @@ public class GeneralCase {
 		} catch (GraphParseException e) {
 			e.printStackTrace();
 		}
-		process_data.readDataFile("part-00002-of-00500.csv",graph);
+		//process_data.readDataFile("part-00002-of-00500.csv",graph);
+
+		Node node;
+		double cap, opcost;
+		int config;
+    		ArrayList<Double> capacity = new ArrayList<Double>();
+    		ArrayList<Integer> k = new ArrayList<Integer>();
+    		ArrayList<Double> opencost = new ArrayList<Double>();
+    		for(int i=0;i<parameters_generator.AP_NUM;i++){
+    			node = graph.getNode(i);
+    			cap = node.getAttribute("capacity");
+    			config = node.getAttribute("configNum");
+    			opcost = node.getAttribute("opencost");
+    			capacity.add(cap);
+    			k.add(config);
+    			opencost.add(opcost);
 
 		GeneralCase gc = new GeneralCase();
 		try{
     		gc.run(graph
-    			,parameters_generator.cloudletCapacity(parameters_generator.CLOUDLET_NUM, 8000, 12000)
-    			,parameters_generator.typeK(parameters_generator.CLOUDLET_NUM, 10, 20)
-    			,parameters_generator.cost(parameters_generator.AP_NUM)
+    			,capacity
+    			,k
+    			,opencost
     			,"GeneralCase");
     	}
-    	catch(Exception x){
+    	catch(IOException x){
     		x.printStackTrace();
     	}
+		}
 	}
 }
